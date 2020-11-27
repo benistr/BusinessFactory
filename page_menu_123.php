70aaf56a7935045738095764aeb93b6128f45880
benistr <benjamin.str@gmail.com>
2020-11-27 16:29:51 +0100

    css ok

--- page_menu_123.php
+++ page_menu_123.php
@@ -1,6 +1,6 @@
 
 
-<a href="https://jeromebparis.wixsite.com/website/inscription-equipe"> INSCRIPTION </a>
+<a class="inscription" href="https://jeromebparis.wixsite.com/website/inscription-equipe">> INSCRIPTION <</a>
 
 
 <?php
